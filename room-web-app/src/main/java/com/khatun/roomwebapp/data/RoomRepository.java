aba74bac47ef203b6322de47f775d15cc6ce968f
TuanDao2002 <s3877347@rmit.edu.vn>
2022-02-07 17:25:16 +0700

    how data repository is implemented

--- room-web-app/src/main/java/com/khatun/roomwebapp/data/RoomRepository.java
+++ room-web-app/src/main/java/com/khatun/roomwebapp/data/RoomRepository.java
@@ -3,7 +3,7 @@ package com.khatun.roomwebapp.data;
 import com.khatun.roomwebapp.models.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// JpaRepository<{model name}, {datatype of primary key of the mapping database to the model}
+// JpaRepository<{model name}, {datatype of primary key of the mapping database to the model}>
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
 }
--- room-web-app/src/main/java/com/khatun/roomwebapp/data/StaffRepository.java
+++ room-web-app/src/main/java/com/khatun/roomwebapp/data/StaffRepository.java
@@ -3,7 +3,7 @@ package com.khatun.roomwebapp.data;
 import com.khatun.roomwebapp.models.StaffMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// JpaRepository<{model name}, {datatype of primary key of the mapping database to the model}
+// JpaRepository<{model name}, {datatype of primary key of the mapping database to the model}>
 public interface StaffRepository extends JpaRepository<StaffMember, String> {
 
 }
